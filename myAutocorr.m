1399585cb87a00d7a4a964c59ff94c2547a3c176
Oskar <osclars@student.chalmers.se>
2014-12-05 08:41:40 +0100

    fixed autocorr function in matlab

--- myAutocorr.m
+++ myAutocorr.m
@@ -3,25 +3,23 @@ clf
 data = load('energy.data');
 
 localEnergy=data(:,1);
-meanEnergy=data(:,2);
 %% Energy autocorr
-%plot([0 1200],[0.135 0.135],'k--')
+plotMax=150;
+plot([0 plotMax],[0.135 0.135],'k--')
 hold on
-%plot([0 1200],[-0.135 -0.135],'k--')
+plot([0 plotMax],[-0.135 -0.135],'k--')
 
-tmp=autocorr(meanEnergy,1200,[],3);
-plot(tmp,'color','blue')
 str={'x=461','y=0.1'};
 
-tmp2=autocorr(localEnergy,1200,[],3);
+tmp2=autocorr(localEnergy,plotMax,[],3);
 
-plot(tmp2,'color','red')
-%axis([0,1200,-0.5,1.1]);
-set(gca,'fontsize',16);
-xlabel('Number of time steps','fontsize',16)
-ylabel('Correlation','fontsize',16)
-title('Temperature correlation functions','fontsize',16)
-legend('Correlation limit', '','773K','973K')
+plot(tmp2,'color','red','LineWidth',2)
+axis([0,plotMax,-0.5,1.1]);
+set(gca,'fontsize',20);
+xlabel('Number of time steps','fontsize',20)
+ylabel('Correlation','fontsize',20)
+title('Energy correlation function','fontsize',20)
+legend('Correlation limit', '','Local energy')
 grid on
 hold off
 
